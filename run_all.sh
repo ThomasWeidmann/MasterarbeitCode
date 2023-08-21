f8e16245649352b850778281e74aa48e9e222eeb
thomas.weidmann@outlook.com <thomas.weidmann@outlook.com>
2023-08-21 23:13:53 +0200

    bash skript

--- run_all.sh
+++ run_all.sh
@@ -6,5 +6,6 @@ for n in {1000000,10000000,100000000,1000000000}; do
 		for dist in {50,100,200,300,400,500,600,700,800,900,1000,1200,1500,2000,3000}; do
 			echo "mpirun -np " $p " build/Code ruling_set " $n " " $dist
 			mpirun -np $p build/Code ruling_set $n $dist
-done
-
+		done
+	done
+done 
