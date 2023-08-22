fb809600adf719539263671b39ea662a02d5f905
thomas.weidmann@outlook.com <thomas.weidmann@outlook.com>
2023-08-22 11:50:39 +0200

    bash skript updated

--- run_all.sh
+++ run_all.sh
@@ -6,7 +6,7 @@ for n in {1000000,10000000,100000000,1000000000}; do
 		mpirun -np $p build/Code sequential $n
 		mpirun -np $p build/Code pointer_doubling $n
 		for dist in {50,100,200,300,400,500,600,700,800,900,1000,1200,1500,2000,3000}; do
-			echo "mpirun -np " $p " build/Code ruling_set " $n " " $dist
+			echo "mpirun -np 1 build/Code ruling_set " $n " " $dist
 			mpirun -np $p build/Code ruling_set $n $dist
 		done
 	done
