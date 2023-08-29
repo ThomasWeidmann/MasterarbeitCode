5b7753b431a9bc02c89fa9dbd1ee0a6fb8d72786
thomas.weidmann@outlook.com <thomas.weidmann@outlook.com>
2023-08-29 22:17:31 +0200

    skript geupdatet

--- run_all.sh
+++ run_all.sh
@@ -2,7 +2,7 @@
 
 
 
-for n in {1000000}; do
+for n in {1000000,10000000}; do
 	for p in {2,4,6,8,10,12,16,20,24,28,32,36,40,44,48,52,56,60,65,70,75,80}; do
 		for dist in {8,10,15,20,30,40,50,60,70,80,90,100,150}; do
 			echo "mpirun -np " $p " build/Code ruling_set " $n " " $dist
